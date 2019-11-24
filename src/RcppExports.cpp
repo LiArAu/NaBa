650d9b71a333f720c2d2e42ba827fc7f694ec8ac
Yajing Li <yajingli@umich.edu>
2019-11-23 20:29:00 -0500

    update documentation

--- src/RcppExports.cpp
+++ src/RcppExports.cpp
@@ -5,6 +5,20 @@
 
 using namespace Rcpp;
 
+// llh_cat
+List llh_cat(CharacterMatrix all_catpredictors, CharacterVector varname, CharacterVector y, int laplace);
+RcppExport SEXP _NaBa_llh_cat(SEXP all_catpredictorsSEXP, SEXP varnameSEXP, SEXP ySEXP, SEXP laplaceSEXP) {
+BEGIN_RCPP
+    Rcpp::RObject rcpp_result_gen;
+    Rcpp::RNGScope rcpp_rngScope_gen;
+    Rcpp::traits::input_parameter< CharacterMatrix >::type all_catpredictors(all_catpredictorsSEXP);
+    Rcpp::traits::input_parameter< CharacterVector >::type varname(varnameSEXP);
+    Rcpp::traits::input_parameter< CharacterVector >::type y(ySEXP);
+    Rcpp::traits::input_parameter< int >::type laplace(laplaceSEXP);
+    rcpp_result_gen = Rcpp::wrap(llh_cat(all_catpredictors, varname, y, laplace));
+    return rcpp_result_gen;
+END_RCPP
+}
 // pdist_num
 List pdist_num(NumericMatrix all_numpredictors, CharacterVector varname, CharacterVector y, CharacterVector type);
 RcppExport SEXP _NaBa_pdist_num(SEXP all_numpredictorsSEXP, SEXP varnameSEXP, SEXP ySEXP, SEXP typeSEXP) {
@@ -19,22 +33,10 @@ BEGIN_RCPP
     return rcpp_result_gen;
 END_RCPP
 }
-// timesTwo
-NumericVector timesTwo(CharacterMatrix x, Function f);
-RcppExport SEXP _NaBa_timesTwo(SEXP xSEXP, SEXP fSEXP) {
-BEGIN_RCPP
-    Rcpp::RObject rcpp_result_gen;
-    Rcpp::RNGScope rcpp_rngScope_gen;
-    Rcpp::traits::input_parameter< CharacterMatrix >::type x(xSEXP);
-    Rcpp::traits::input_parameter< Function >::type f(fSEXP);
-    rcpp_result_gen = Rcpp::wrap(timesTwo(x, f));
-    return rcpp_result_gen;
-END_RCPP
-}
 
 static const R_CallMethodDef CallEntries[] = {
+    {"_NaBa_llh_cat", (DL_FUNC) &_NaBa_llh_cat, 4},
     {"_NaBa_pdist_num", (DL_FUNC) &_NaBa_pdist_num, 4},
-    {"_NaBa_timesTwo", (DL_FUNC) &_NaBa_timesTwo, 2},
     {NULL, NULL, 0}
 };
 
--- src/pdist_num.cpp
+++ src/pdist_num.cpp
@@ -40,30 +40,7 @@ List pdist_num(NumericMatrix all_numpredictors,CharacterVector varname,Character
     return ret;
   }
 
-List llh_cat(CharacterMatrix all_catpredictors,CharacterVector varname,CharacterVector y,int laplace){
-  int nvar=all_catpredictors.ncol();
-  List ret;
-  Function f("table");
 
-  for (int k =0; k<nvar ; k++){
-    CharacterVector predictor=all_catpredictors(_,k);
-    NumericMatrix table=f(y,predictor);
-    NumericMatrix llh(table.nrow(),table.ncol());
-    CharacterVector ch = colnames(table);
-    colnames(llh) = ch;
-    for (int i=0; i<table.nrow();i++){
-      int rowsum=0;
-      for (int j=0; j<table.ncol();j++){
-        llh(i,j)=table(i,j)+laplace;
-        rowsum+=table(i,j);
-      }
-      llh(i,_)=llh(i,_)/(rowsum+laplace*table.ncol());
-    }
-    String name=varname[k];
-    ret[name]=llh;
-  }
-  return ret;
-}
 
 
 
