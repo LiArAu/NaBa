512d75c4021e255063d05285ef8bcb09aa379daf
Yajing Li <yajingli@umich.edu>
2019-11-25 10:28:50 -0500

    add e1071 in testthat

--- tests/testthat/test-naba.R
+++ tests/testthat/test-naba.R
@@ -2,8 +2,9 @@ test_that("predict_naBa works", {
   data(mood)
   x=mood[,1:5]
   y=mood[,6]
-  newdata=mood[1:10,1:5]
+  newdata=mood[1:200,1:5]
   prior=NaBa::Info_prior(x,y)
-  expect_equal(predict_naBa(prior,newdata,"class"), factor(c("excellent",
-  "excellent","bad","excellent","excellent","bad","excellent","excellent","bad","bad")))
+  prior2=e1071::naiveBayes(x,y)
+  expect_equal(predict_naBa(prior,newdata,"class"),predict(prior2,newdata,"class"))
 })
+
