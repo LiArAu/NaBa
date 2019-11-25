db6123ae9418e33e103edd09457b645076e0477d
Yajing Li <yajingli@umich.edu>
2019-11-25 15:44:28 -0500

    update test case

--- tests/testthat/test-naba.R
+++ tests/testthat/test-naba.R
@@ -2,7 +2,24 @@ test_that("predict_naBa works", {
   data(mood)
   x=mood[,1:5]
   y=mood[,6]
-  newdata=mood[1:200,1:5]
+  newdata=mood[1:100,1:5]
+  prior=NaBa::Info_prior(x,y)
+  prior2=e1071::naiveBayes(x,y)
+  expect_equal(predict_naBa(prior,newdata,"class"),predict(prior2,newdata,"class"))
+  expect_equal(predict_naBa(prior,newdata,"raw"),predict(prior2,newdata,"raw"))
+
+  data(iris)
+  x=iris[,1:4]
+  y=iris[,5]
+  newdata=iris[1:50,1:4]
+  prior=NaBa::Info_prior(x,y)
+  prior2=e1071::naiveBayes(x,y)
+  expect_equal(matrix(predict_naBa(prior,newdata,"class")),matrix(predict(prior2,newdata,"class")))
+
+  data(Titanic)
+  x=Titanic[,1:3]
+  y=Titanic[,4]
+  newdata=Titanic[1:10,1:3]
   prior=NaBa::Info_prior(x,y)
   prior2=e1071::naiveBayes(x,y)
   expect_equal(predict_naBa(prior,newdata,"class"),predict(prior2,newdata,"class"))
